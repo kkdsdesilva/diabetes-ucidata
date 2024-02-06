09357cea6b972355312dc5664e48bd7379ae9daf
kkdsdesilva <kkdsdesilva@icloud.com>
2024-02-06 15:59:01 -0700

    Update Logistic regression model with additional parameters

--- src/models/Logistic.py
+++ src/models/Logistic.py
@@ -3,11 +3,11 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-def LogReg(X_train, X_test, y_train, y_test, pen='l2', max_iter=1000):
+def LogReg(X_train, X_test, y_train, y_test, pen='l2', max_iter=1000, C=1.0, solver='lbfgs'):
     '''Returns the logistic regression model and print the predictions.'''
     
     # create the logistic
-    logreg = LogisticRegression(penalty=pen, n_jobs=-1, max_iter=max_iter)
+    logreg = LogisticRegression(penalty=pen, n_jobs=-1, max_iter=max_iter, C=C, solver=solver)
 
     # fit the model
     logreg.fit(X_train, y_train)
@@ -25,4 +25,8 @@ def LogReg(X_train, X_test, y_train, y_test, pen='l2', max_iter=1000):
     print('Test accuracy: ', test_acc)
 
     # return the model
-    return logreg
+    return logreg, y_train_pred, y_test_pred
+
+if __name__ == '__main__':
+    # test the logistic regression model
+    logreg, y_train_pred, y_test_pred = LogReg(X_train, X_test, y_train, y_test)
