4791c9f17ddf467e876f4b0978cec3ddbba751bb
kkdsdesilva <kkdsdesilva@icloud.com>
2024-02-07 16:21:50 -0700

    Add .gitignore file and update data files, update train_Logistic with mlflow

--- scripts/train_Logistic.py
+++ scripts/train_Logistic.py
@@ -1,15 +1,17 @@
 # script for training logistic regression model
 
+# import libraries
 import sys
 import os
 import pandas as pd
+import mlflow
+import mlflow.sklearn
 
 # append the path
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.join(cur_dir, '../')
 sys.path.append(root_dir)
 
-# import libraries
 from src.data.load_data import load_data
 from src.data.data_scaling import standardize_data, normalize_data
 from src.features.feature_labeling import label_encode, one_hot_encode
@@ -17,24 +19,44 @@ from src.data.split_data import split_data
 from src.models.Logistic import train_Logistic
 from src.models.evaluate import evaluate_model
 
-# import data
-data = load_data()
+def load_and_preprocess_data():
+    """Load and preprocess the dataset."""
+    data = load_data()
+    data = label_encode(data, 'readmitted', {'NO': 0, '>30': 1, '<30': 1})
+    data = one_hot_encode(data)
+    return data
 
-# label encode the categorical features
-data = label_encode(data, 'readmitted', {'NO': 0, '>30': 1, '<30': 1})
+def log_model_metrics(logreg, X_train, X_test, y_train, y_test):
+    """Log model metrics to MLflow."""
+    train_acc, test_acc = evaluate_model(logreg, X_train, X_test, y_train, y_test)
+    mlflow.log_metric("train_accuracy", train_acc)
+    mlflow.log_metric("test_accuracy", test_acc)
 
-# one hot encode the categorical features
-data = one_hot_encode(data)
+def main():
+    # Load and preprocess data
+    data = load_and_preprocess_data()
 
-# split the data
-X_train, X_test, y_train, y_test = split_data(data, 'readmitted')
+    # Split the data
+    X_train, X_test, y_train, y_test = split_data(data, 'readmitted')
 
-# standardize the data
-X_train = standardize_data(X_train)
-X_test = standardize_data(X_test)
+    # Standardize the data
+    X_train = standardize_data(X_train)
+    X_test = standardize_data(X_test)
 
-# train the model
-logreg = train_Logistic(X_train, y_train)
+    # Train the model
+    logreg = train_Logistic(X_train, y_train, max_iter=1500, C=1)
 
-# evaluate the model
-evaluate_model(logreg, X_train, X_test, y_train, y_test)
+    # Log model and metrics to MLflow
+    mlflow.set_tracking_uri("file://" + os.path.join(cur_dir, '..', 'experiments', 'logistic_regression', 'mlruns'))
+
+    # Set the experiment
+    mlflow.set_experiment('logistic_regression_experiment')
+
+    with mlflow.start_run():
+        mlflow.sklearn.log_model(logreg, "logistic_regression_model")
+        mlflow.log_params({"penalty": logreg.get_params()['penalty'], "C": logreg.get_params()['C']})
+        log_model_metrics(logreg, X_train, X_test, y_train, y_test)
+
+
+if __name__ == '__main__':
+    main()
