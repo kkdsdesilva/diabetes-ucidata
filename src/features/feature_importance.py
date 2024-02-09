d4a493a65a97804feff6c787cb8414a76d664954
kkdsdesilva <kkdsdesilva@icloud.com>
2024-02-09 13:27:37 -0700

    Update feature_selection.cpython-311.pyc and test_notebook.ipynb, add pick_top_k_features function

--- src/features/feature_importance.py
+++ src/features/feature_importance.py
@@ -13,6 +13,7 @@ def feature_importance(model, data):
     # return the feature importance
     return importance
 
+
 # feature importance for logistic regression
 def feature_importance_logreg(model, data):
     '''Returns the feature importance of the logistic regression model.'''
@@ -22,3 +23,20 @@ def feature_importance_logreg(model, data):
     
     # return the feature importance
     return importance
+
+
+# pick the top k features
+def pick_top_k_features(data, importance, k=10):
+    '''Returns the top k features.'''
+
+    # get the absolute value of the importance
+    importance['importance'] = abs(importance['importance'])
+    
+    # pick the top k features
+    top_k_features = importance.head(k)
+
+    # subset data with the top k features
+    data_top = data[top_k_features.index]
+
+    # return the top k features
+    return data_top
