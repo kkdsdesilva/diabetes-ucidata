1cbbddd0fddb686bafcc2a1b4d05899fb4d1e3ac
hasi03 <pkheranda@gmail.com>
2024-03-15 14:06:14 -0700

    Added varying Learning rate option

--- src/models/NN.py
+++ src/models/NN.py
@@ -12,7 +12,7 @@ from tqdm.keras import TqdmCallback
 
     
 
-def train_NN(layers_config, X_train, y_train, input_dim, n_ini=64, epochs=10, batch_size=32, validation_split=0.1, learning_rate=0.01):
+def train_NN(layers_config, X_train, y_train, input_dim, n_ini=64, epochs=10, batch_size=32, validation_split=0.1, lr=['constant',0.01]):
     """
     Create a sequential model based on the provided layers configuration.
 
@@ -30,11 +30,30 @@ def train_NN(layers_config, X_train, y_train, input_dim, n_ini=64, epochs=10, ba
         model.add(Dense(units, activation=activation))
     
     model.add(Dense(1, activation='sigmoid'))  # Example output layer
-    model.compile(optimizer=Adam(learning_rate=0.01), loss='binary_crossentropy', metrics=['accuracy', Recall(name='recall')])
 
+
+    # Set the learning rate scheduler
+    if lr[0] == 'varying':
+
+      lr_schedule = lr[1]  
+      #lr_schedule = tf.keras.callbacks.LearningRateScheduler(
+      #  lambda epoch: 1e-8 * 10**(epoch / 20))
+      
+      # Instantiate the Adam optimizer
+      adam_optimizer = Adam()
+
+      model.compile(optimizer=adam_optimizer, loss='binary_crossentropy', metrics=['accuracy', Recall(name='recall')])
+
+      history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=0,
+                          callbacks=[lr_schedule, TqdmCallback(verbose=1)])
+
+    else:
+       
     # train the model
-    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=0,
-                        callbacks=[TqdmCallback(verbose=1)])
+      model.compile(optimizer=Adam(learning_rate=lr[1]), loss='binary_crossentropy', metrics=['accuracy', Recall(name='recall')])
+
+      history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=0,
+                          callbacks=[TqdmCallback(verbose=1)])
     
     #early_stopping = EarlyStopping(monitor='val_recall', mode='max', patience=5, restore_best_weights=True)
    # history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=0,
