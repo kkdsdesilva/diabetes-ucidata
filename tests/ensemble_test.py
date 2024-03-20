869ef0430db7ccf053e7a3299576ab64a138bcb7
kkdsdesilva <kkdsdesilva@icloud.com>
2024-03-20 10:42:41 -0700

    change ensemble_test file

--- tests/ensemble_test.py
+++ tests/ensemble_test.py
@@ -25,17 +25,12 @@ cols = ['race', 'gender', 'age', 'admission_type_id',
     'number_diagnoses', 'readmitted']
 
 # Load and preprocess data
-data = load_data()[cols]
+data_nn = engineer_features(load_data(), 'readmitted', one_hot=True)
+data_rf = engineer_features(load_data(), 'readmitted', one_hot=False)
 
 # Split the data
-train, test = train_test_split(data, test_size=0.2, random_state=3, stratify=data['readmitted'])
-
-# engineer features for the two models
-train_nn = engineer_features(train, 'readmitted', one_hot=True)
-test_nn = engineer_features(test, 'readmitted', one_hot=True)
-
-train_rf = engineer_features(train, 'readmitted', one_hot=False)
-test_rf = engineer_features(test, 'readmitted', one_hot=False)
+train_nn, test_nn = train_test_split(data_nn, test_size=0.1, random_state=3, stratify=data_nn['readmitted'])
+train_rf, test_rf = train_test_split(data_rf, test_size=0.1, random_state=3, stratify=data_rf['readmitted'])
 
 print(train_nn.shape, test_nn.shape, train_rf.shape, test_rf.shape)
 
@@ -53,19 +48,21 @@ X_train_rf = standardize_data(X_train_rf)
 X_test_rf = standardize_data(X_test_rf)
 
 # train random forest model
-rf = train_RandomForest(X_train_rf, y_train_rf, n_estimators=100, max_depth=None, min_samples_split=2, min_samples_leaf=1, random_state=0, criterion='gini')
+rf = train_RandomForest(X_train_rf, y_train_rf, n_estimators=100, max_depth=5, min_samples_split=2, min_samples_leaf=1, random_state=0, criterion='gini')
 
 # train neural network model
 nn, history = train_NN(X_train_nn, y_train_nn, input_dim=X_train_nn.shape[1], epochs=10, batch_size=32, validation_split=0, learning_rate=0.01)
 
 # predict using the random forest model
-y_pred_rf = rf.predict(X_test_rf)
+y_pred_rf = rf.predict(X_test_rf).reshape(-1)
 
 # predict using the neural network model
-y_pred_nn = (nn.predict(X_test_nn) > 0.5).astype('int32')
+y_pred_nn = (nn.predict(X_test_nn) > 0.5).astype('int32').reshape(-1)
 
 # use both models to predict. If at least one model predict 1, then the ensemble model predicts 1, else 0
 y_pred_ensemble = (y_pred_rf + y_pred_nn) > 0
+y_pred_ensemble = 
+
 
 # print the accuracy of the ensemble model
 print("Accuracy of the ensemble model: ", sum(y_pred_ensemble == y_test_rf) / len(y_test_rf))
